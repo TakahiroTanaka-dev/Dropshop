17f80cbde8f385ec33e7b31d1c6520f6474894d0
TakahiroTanaka-dev <takahiro.forwork1130@gmail.com>
2020-12-09 22:11:12 +0900

    titleタグをPHPで書き換え

--- themes/dropmall/header.php
+++ themes/dropmall/header.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width,initial-scale=1" />
   <meta name="keywords" content="共通キーワード" />
   <meta name="description" content="共通ディスクリプション" />
-  <title>PACIFIC MALL DEVELOPMENT</title>
+  <title><?php wp_get_document_title(); ?></title>
   <link rel="shortcut icon" href="<?php echo get_template_directory_uri();  ?>/assets/images/common/favicon.ico" />
   <link href="https://fonts.googleapis.com/earlyaccess/notosansjapanese.css" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/css?family=Vollkorn:400i" rel="stylesheet" />
