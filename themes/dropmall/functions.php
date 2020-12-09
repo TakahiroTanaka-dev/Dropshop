25ada96c78c1a5d5715e89ea11934c0ebde15873
TakahiroTanaka-dev <takahiro.forwork1130@gmail.com>
2020-12-10 01:00:23 +0900

    wp_headがwp-headになっていたので修正

--- themes/dropmall/functions.php
+++ themes/dropmall/functions.php
@@ -1,7 +1,11 @@
 <?php
 
-add_action('wp_enqueue_scripts', function (){
+function my_enqueue_scripts(){
   wp_enqueue_script('jquery');
   wp_enqueue_script('bundle_js', get_template_directory_uri().'/assets/js/bundle.js', array());
   wp_enqueue_style('my_styles', get_template_directory_uri().'/assets/css/style.css', array());
-});
+}
+
+add_action('wp_enqueue_scripts', 'my_enqueue_scripts');
+
+?>
