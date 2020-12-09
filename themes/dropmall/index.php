72eb0e061cf6e441ea2dc4ab0d93ef5bdc7ce7f4
TakahiroTanaka-dev <takahiro.forwork1130@gmail.com>
2020-12-09 21:21:26 +0900

    chnaged the all file paths

--- themes/dropmall/index.php
+++ themes/dropmall/index.php
@@ -6,12 +6,12 @@
   <meta name="keywords" content="共通キーワード" />
   <meta name="description" content="共通ディスクリプション" />
   <title>PACIFIC MALL DEVELOPMENT</title>
-  <link rel="shortcut icon" href="./assets/images/common/favicon.ico" />
+  <link rel="shortcut icon" href="<?php echo get_template_directory_uri();  ?>/assets/images/common/favicon.ico" />
   <link href="https://fonts.googleapis.com/earlyaccess/notosansjapanese.css" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/css?family=Vollkorn:400i" rel="stylesheet" />
   <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();  ?>/assets/css/styles.css" />
-  <script type="text/javascript" src="./assets/js/jquery-3.3.1.min.js"></script>
-  <script type="text/javascript" src="./assets/js/bundle.js"></script>
+  <script type="text/javascript" src="<?php echo get_template_directory_uri();  ?>/assets/js/jquery-3.3.1.min.js"></script>
+  <script type="text/javascript" src="<?php echo get_template_directory_uri();  ?>/assets/js/bundle.js"></script>
 </head>
 <body>
   <div class="container">
