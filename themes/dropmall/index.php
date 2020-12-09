f2262af3964a91fd32e7d1da301f36f223d3fc50
TakahiroTanaka-dev <takahiro.forwork1130@gmail.com>
2020-12-09 21:13:04 +0900

    changed the path for stylesheet

--- themes/dropmall/index.php
+++ themes/dropmall/index.php
@@ -9,7 +9,7 @@
   <link rel="shortcut icon" href="./assets/images/common/favicon.ico" />
   <link href="https://fonts.googleapis.com/earlyaccess/notosansjapanese.css" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/css?family=Vollkorn:400i" rel="stylesheet" />
-  <link rel="stylesheet" type="text/css" href="./assets/css/styles.css" />
+  <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();  ?>/assets/css/styles.css" />
   <script type="text/javascript" src="./assets/js/jquery-3.3.1.min.js"></script>
   <script type="text/javascript" src="./assets/js/bundle.js"></script>
 </head>
