36db04415d619bef5fa56e5ba63ba78956428285
TakahiroTanaka-dev <takahiro.forwork1130@gmail.com>
2020-12-15 23:35:01 +0900

    ループ処理の記述を修正

--- themes/dropmall/page.php
+++ themes/dropmall/page.php
@@ -3,8 +3,10 @@
 <div class="page-main">
   <div class="lead-inner">
   <?php if(have_posts()): ?>
-    <?php the_post(); ?>
-    <?php the_content(); ?>
+    <?php while(have_posts()): ?>
+      <?php the_post(); ?>
+      <?php the_content(); ?>
+    <?php endwhile; ?>
   <?php endif; ?>
   </div>
 </div>
